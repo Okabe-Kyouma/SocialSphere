89800bc81523bcbd40ab3957124a924280c85860
Okabe-Kyouma <ayushpal5432@gmail.com>
2024-04-30 18:08:25 +0530

    FriendListLogicImplemented

--- routes/friends.js
+++ routes/friends.js
@@ -1,12 +1,26 @@
 const router = require('express').Router();
-const User = require('../Models/user');
+const {User} = require('../Models/user');
 
 
-router.get('/friends',(req,res)=>{
+router.get('/friends',async(req,res)=>{
 
     if(req.isAuthenticated()){
 
-        const friendArray = req.user.friends;
+        const userIds = req.user.friends;
+
+        const friendArray = [];
+
+        for(const id of userIds){
+            try{
+                const user = await User.findById(id);
+                if(user){
+                    friendArray.push(user);
+                }
+            }
+            catch(err){
+                console.log(err);
+            }
+        }
 
         res.render('friends',{friendArray});
 
@@ -18,6 +32,33 @@ router.get('/friends',(req,res)=>{
 
 })
 
+
+router.get('/Unfollow/:userId',async(req,res)=>{
+
+    if(req.isAuthenticated()){
+
+    const currentUser = req.user;
+
+    const {userId} = req.params;
+
+     console.log(userId);
+
+     const indexToRemove = currentUser.friends.indexOf(userId);
+
+     if(indexToRemove!==-1){
+        currentUser.friends.splice(indexToRemove,1);
+     }
+
+
+     await currentUser.save();
+
+     res.redirect('/home');
+    }
+    else{
+        res.redirect('/');
+    }
+
+})
 
 
 
